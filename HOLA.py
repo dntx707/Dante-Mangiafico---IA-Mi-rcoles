7b00ea5caaf880933683bf0d4addef87e1fcbfe7
dantemangia09 <mangiaficodante09@gmail.com>
2026-01-18 01:29:21 -0300

    Update HOLA.py

--- HOLA.py
+++ HOLA.py
@@ -2,7 +2,7 @@ import streamlit as st
 from groq import Groq
 
 st.set_page_config(page_title="Mi chat de IA", page_icon="🤖")
-st.title("Bienvenido a MangiAI")
+st.title("¡Bienvenido a MangiAI!")
 
 MODELOS = ['llama-3.1-8b-instant', 'llama-3.3-70b-versatile', 'deepseek-r1-distill-llama-70b']
 
@@ -54,5 +54,6 @@ if mensaje:
 
 
 area_chat()
+
 
 
